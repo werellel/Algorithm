dd65f74f3c210c6f0069572d8030e68d77f715df
unknown <werellel@gmail.com>
2020-06-23 23:55:58 +0900

    arrays

--- hackerrank/arrays/array_manipulation.py
+++ hackerrank/arrays/array_manipulation.py
@@ -7,7 +7,25 @@ import re
 import sys
 
 # Complete the arrayManipulation function below.
+def naive_arrayManipulation(n, queries):
+    return_list = [0]*n
+    for query in queries:
+        for i in range(query[0]-1, query[1]):
+            return_list[i] += query[2]
+    return max(return_list)
+
+# Complete the arrayManipulation function below.
 def arrayManipulation(n, queries):
+    l = [0]*(n+1)
+    for a, b, k in queries:
+        l[a-1] += k
+        l[b] -= k;
+    max_value = a = 0
+    for i in l:
+       a+=i;
+       if max_value<a:
+            max_value=a;
+    return max_value
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
