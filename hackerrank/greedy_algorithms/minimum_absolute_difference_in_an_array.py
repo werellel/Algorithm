f15cdaa02210b6f2f8b68b7e30e88f9f41faafeb
unknown <werellel@gmail.com>
2020-06-30 23:41:49 +0900

    greedy_algorithms_minimum_absolute_difference_in_an_array

---
+++ hackerrank/greedy_algorithms/minimum_absolute_difference_in_an_array.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    data = set(abs(x-y) for x,y in zip(arr,arr[1:]))
+    return min(data)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
