d89147d7ddf16ac3be141cf09c6e68d43de03483
unknown <werellel@gmail.com>
2020-07-02 23:03:40 +0900

    greedy_algorithms_greedy_florist

---
+++ hackerrank/greedy_algorithms/greedy_florist.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(n, k, c):
+    cost = 0
+    c = sorted(c, reverse=True)
+    for i in range(n):
+        cost += (i // k + 1) * c[i]
+    return cost
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(n, k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
